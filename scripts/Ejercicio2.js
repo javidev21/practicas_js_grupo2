28735123f5ca14f15b4b1269822879637afa2728
Joel Quimis <quimisjoedl@gmail.com>
2025-06-01 18:00:04 -0500

    Resuelto

--- scripts/Ejercicio2.js
+++ scripts/Ejercicio2.js
@@ -4,3 +4,14 @@ Para ser elegible para un subsidio, debes tener al menos 18 años y tus ingresos
 Escribe un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario es 
 elegible para el subsidio o no. (10p)
 */
+
+// Solicitar datos al usuario
+let edad = parseInt(prompt("¿Cuál es tu edad?"));
+let ingresos = parseFloat(prompt("¿Cuáles son tus ingresos mensuales en euros?"));
+
+// Verificar la elegibilidad para el subsidio
+if (edad >= 18 && ingresos >= 500 && ingresos <= 2000) {
+    console.log("Eres elegible para el subsidio.");
+} else {
+    console.log("No eres elegible para el subsidio.");
+}
