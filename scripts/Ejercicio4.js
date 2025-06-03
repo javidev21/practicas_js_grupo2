5798cc286777a20a6bfa16137688372baf8a7828
javidev21 <ingjaviersalas@gmail.com>
2025-06-02 23:27:28 -0600

    Ejercicio4

--- scripts/Ejercicio4.js
+++ scripts/Ejercicio4.js
@@ -7,5 +7,18 @@
   y un mensaje si el código no es válido. (10p)
   */
 
-let total = parseFloat(prompt("¿Cuál es el total de su compra?));
-let descuento = parseFloat(prompt("¿Tiene un código de descuento?"));
+let total = parseFloat(prompt("¿Cuál es el total de su compra? "));
+if(isNaN(total) || total < 0){
+  console.log("Por favor ingrese un número válido")
+}else{
+  let descuento = prompt("¿Tiene un código de descuento? ");
+if (descuento.toUpperCase()==="DESCUENTO10"){
+  total = total *.9;
+  console.log(`Su total con un 10% de descuento es: $ ${total.toFixed(2)}`);
+}else if (descuento.toUpperCase()==="DESCUENTO20"){
+  total = total *.8;
+  console.log(`Su total con un 20% de descuento es $ ${total.toFixed(2)}`);
+}else{
+  console.log(`El código de descuento no es válido, su total es $ ${total.toFixed(2)}`);
+}
+}
